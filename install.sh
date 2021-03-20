fb8e3300804edee92dc855cbf5b51523893fd052
Sergii Pylypenko <x.pelya.x@gmail.com>
2021-03-20 22:24:01 +0200

    Added freedoom.wad, added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ -e make-kaios-install/Makefile ] || git submodule update --init --recursive
+
+echo "Download XULRunner from http://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/18.0.2/sdk/ "
+
+export LD_LIBRARY_PATH=~/bin/xulrunner-sdk/bin
+
+
+FOLDER=`pwd`/out
+[ -z "$FOLDER" ] && FOLDER=release
+ID=doom
+
+adb push ${FOLDER}/application.zip /data/local/tmp/b2g/$ID/application.zip || exit 1
+
+cd build/make-kaios-install
+
+make FOLDER=$FOLDER ID=$ID install || exit 1
